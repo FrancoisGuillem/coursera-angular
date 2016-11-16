85b23227768fb9ad2f284edf5231761c5302a0f6
françois GUILLEM <francois.guillem@rte-france.com>
2016-11-16 14:01:34 +0100

    solution for module 2 finished

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,16 +1,50 @@
 (function() {
   angular.module("ShoppingListCheckOff", [])
-    .controller("ToBuyController")
-    .controller("AlreadyBoughtController");
+    .controller("AlreadyBoughtController", AlreadyBoughtController)
+    .controller("ToBuyController", ToBuyController)
+    .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
-  ToBuyController.$inject = ["$scope"];
-  AlreadyBoughtController.$inject = ["$scope"];
-
-  function ToBuyController($scope) {
-
+  ToBuyController.$inject = ["ShoppingListCheckOffService"];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
+    toBuy.list = ShoppingListCheckOffService.toBuyList;
+    toBuy.buyItem = ShoppingListCheckOffService.buyItem;
   }
 
-  function AlreadyBoughtController($scope) {
-
+  AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
+    bought.list = ShoppingListCheckOffService.boughtList;
   }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+    service.toBuyList = [];
+    service.boughtList = [];
+
+    service.buyItem = function(index) {
+      var item = service.toBuyList.splice(index, 1)[0];
+      service.boughtList.push(item);
+    };
+
+    // Fill shopping list with random stuff
+    var products = ["cheeses", "cookies", "sausages", "candies", "cakes",
+                    "tomatos", "strawberries", "cucumbers", "breads", "biers", "vines"]
+    var countries = ["", "french", "german", "spanish", "indian", "italian",
+                     "chinese", "japanese", "mexican", "moon"]
+    var adjectives = ["", "spicy", "sweet", "bitter", "red", "blue",
+                      "yellow", "green", "soft", "hard", "weird", "hydrophobic"]
+
+    function randomInteger(nmax) {return Math.floor(Math.random() * nmax)}
+    function randomElem(array) {return array[randomInteger(array.length)]}
+
+    for (var i=0; i < 10; i++) {
+      var item = {
+        name: randomElem(countries) + " " + randomElem(adjectives) + " " + randomElem(products),
+        quantity: randomInteger(19) + 1
+      }
+      service.toBuyList.push(item);
+    }
+  }
+
 })();
