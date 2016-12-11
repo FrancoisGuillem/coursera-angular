18f57015c04f0fbd9f58484158c00dd8124cafc9
cuche27 <guillem_francois@yahoo.fr>
2016-12-11 13:27:00 +0100

    remove fake data

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -5,12 +5,6 @@
   signupController.$inject = ["MenuService", "UserInfoService"];
   function signupController(MenuService, UserInfoService) {
     var ctrl = this;
-    ctrl.user = {
-      firstName: "Tony",
-      lastName: "Stark",
-      email: "tony@avengers.com",
-      phone: "123-123-1234"
-    }
     ctrl.invalidItem = false; // Used to validate field "favourite item"
     ctrl.saved = false // Has information been saved
 
